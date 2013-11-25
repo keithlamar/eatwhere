d9835c401c7bb99c42f07bc6dc449ffff83e98ba
keithlamar <klgdesign@gmail.com>
2013-11-25 13:37:34 -0500

    fix syntax Error

--- js/application.js
+++ js/application.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 
-	$.getJSON('http://api.citygridmedia.com/content/places/v2/search/where?what=restaurant&where=44126&format=json&publisher=10000005460?callback=?'), function(json) {
+	$.getJSON('http://api.citygridmedia.com/content/places/v2/search/where?what=restaurant&where=44126&format=json&publisher=10000005460?callback=?', function(json) {
 		console.log(json);
-	};
+	});
 });
