bf26c24ec734a8697875de6ed3a87d5f2332839d
keithlamar <klgdesign@gmail.com>
2013-12-02 13:58:26 -0500

    Added Maps API for Lat Lon

--- js/application.js
+++ js/application.js
@@ -1,7 +1,29 @@
+
 $(document).ready(function(){
-
-
-	$.getJSON('http://api.citygridmedia.com/content/places/v2/search/where?what=restaurant&where=44126&format=json&publisher=10000005460?callback=?', function(json) {
-		console.log(json);
+var lat;
+var lon;
+var randomPick = function(){
+	var zip = $('#zipCode').val();
+	var geocoder = new google.maps.Geocoder();
+	geocoder.geocode({'address': zip}, function(results, status){
+			lat = results[0].geometry.location.ob;
+			lon = results[0].geometry.location.pb;
+			console.log(lat);
+			console.log(results);
+		
 	});
+	console.log(lat);
+	var radius = $('#milesAway').val();
+	$.getJSON('http://api.citygridmedia.com/content/places/v2/search/latlon?type=restaurant&lat='+lat+'&lon='+lon+'&sort=highestrated&radius='+radius+'&format=json&publisher=10000005460&callback=?', function(json) {
+		console.log(json);
+		});
+	}
+
+$('#main-form').on('click','#submit', function(event){
+	event.preventDefault();
+	randomPick();
+	console.log(lat)
+});
+
+	
 });
